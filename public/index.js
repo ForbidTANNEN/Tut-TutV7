1c19a20f57a5d6adaf026a63e409b20b76bd4dcd
Tannen <Tannenhall@yahoo.com>
2020-10-19 13:41:39 -0700

    Fixed front page and pic and good text

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+
 // Nav Bar shadow
 $(window).scroll(function() {
     if ($(window).scrollTop() > 10) {
@@ -6,32 +7,3 @@ $(window).scroll(function() {
         $('.navbar').removeClass('floatingNav');
     }
 });
-
-
-//overlay
-$( document ).ready(function() {
-  $(".overlay").height($(".half-width-img").height());
-  $(".overlay1").height($(".half-width-img").height());
-});
-$( window ).resize(function() {
-    $(".overlay").height($(".half-width-img").height());
-    $(".overlay1").height($(".half-width-img").height());
-});
-
-$(".overlay").mouseover(function(){
-  $("#tutorText").addClass("show");
-  console.log("lol");
-});
-
-$(".overlay").mouseout(function(){
-  $("#tutorText").removeClass("show");
-})
-
-$(".overlay1").mouseover(function(){
-  $("#tutorText1").addClass("show");
-  console.log("lol");
-});
-
-$(".overlay1").mouseout(function(){
-  $("#tutorText1").removeClass("show");
-})
